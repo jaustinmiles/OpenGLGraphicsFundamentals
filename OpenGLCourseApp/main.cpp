8ac44990eb9a86732af947f9bf62488364ebe099
jaustinmiles <j.austinmiles@outlook.com>
2020-10-24 19:03:53 -0400

    add translation, rotation, and scale by matrix operations

--- OpenGLCourseApp/main.cpp
+++ OpenGLCourseApp/main.cpp
@@ -12,12 +12,23 @@
 #include <glm/gtc/type_ptr.hpp>
 
 const GLint WIDTH = 800, HEIGHT = 600;
+const float toRadians = 3.14159265f / 180.0f;
+
 GLuint VAO, VBO, shader, uniformModel;
 
 bool direction = true;
 float triOffset = 0.0f;
 float triMaxoffset = 0.7f;
 float triIncrement = 0.005f;
+
+float degreeOffset = 0.0f;
+float degreeMaxOffset = 360.0f;
+float degreeIncrement = 2.0f;
+
+bool sizeDirection = true;
+float curSize = 0.4f;
+float maxSize = 0.8f;
+float minSize = 0.1f;
 
 
 // Vertex Shader
@@ -26,11 +37,11 @@ static const char* vShader = "          \n\
                                         \n\
                                         \n\
 layout (location = 0) in vec3 pos;      \n\
-uniform float xMove;                    \n\
+uniform mat4 model;                     \n\
                                         \n\
 void main()                             \n\
 {                                       \n\
-    gl_Position = vec4(0.4 * pos.x + xMove, 0.4 * pos.y, 0.4 * pos.z, 1.0);       \n\
+    gl_Position = model * vec4(pos, 1.0);\n\
 }                                       \n\
 ";
 
@@ -128,7 +139,7 @@ void CompileShaders() {
         return;
     }
 
-    uniformModel = glGetUniformLocation(shader, "xMove");
+    uniformModel = glGetUniformLocation(shader, "model");
 }
 
 
@@ -206,12 +217,41 @@ int main()
             direction = !direction;
         }
 
+        if (degreeOffset > degreeMaxOffset)
+        {
+            degreeOffset = degreeOffset - degreeMaxOffset;
+        }
+        else
+        {
+            degreeOffset += degreeIncrement;
+        }
+
+        if (sizeDirection)
+        {
+            curSize += 0.01f;
+        }
+        else 
+        {
+            curSize -= 0.01f;
+        }
+
+        if (curSize >= maxSize || curSize <= minSize)
+        {
+            sizeDirection = !sizeDirection;
+        }
+
         // Clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
         glUseProgram(shader);
-        glUniform1f(uniformModel, triOffset);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
+        model = glm::rotate(model, degreeOffset * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
+        model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
+
+        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 
         glBindVertexArray(VAO);
 
